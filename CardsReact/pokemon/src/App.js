748acab500303359186b33039de4cca862c434dd
AlexandreFBrito <alexandre_f_brito@estudante.sesisenai.org.br>
2021-12-09 22:22:01 -0300

    Pokedex

---
+++ CardsReact/pokemon/src/App.js
@@ -0,0 +1,32 @@
+import './App.css'
+import Pokemon from './components/pokemon'
+
+function App(){
+
+  return(
+    
+    <>
+      <h1 className='title'>Pokedex</h1>
+
+      <Pokemon 
+              photo="https://i.pinimg.com/originals/96/d2/1d/96d21d738561947f96255e6f12f1754a.png" 
+              name="Bulbasaur" 
+              hp={100}
+              attack={100}
+              defense={100}
+              types={['grass', 'fire', 'water']} 
+              />
+      <Pokemon 
+              photo="https://i.pinimg.com/originals/96/d2/1d/96d21d738561947f96255e6f12f1754a.png" 
+              name="Bulbasaur" 
+              hp={100}
+              attack={100}
+              defense={100}
+              types={['water', 'fire', 'water']} 
+              />              
+    </>
+
+  );
+}
+
+export default App
---
+++ CardsReact/pokemon/src/components/pokemon/index.jsx
@@ -0,0 +1,30 @@
+import { handleGenerateBg } from "../../utils";
+
+const Pokemon = ({photo, name, hp, attack, defense, types}) =>{
+
+    const typeColor = handleGenerateBg(types[0]);
+
+    return(
+        <div className="pokemon" style={{background: typeColor}}>
+        <img src={photo}
+              alt={name}
+              className="pokemon-image"/>
+        
+        <h2>{name}</h2>
+
+        <ul className='pokemon-info'>
+          <li>HP: {hp}</li>
+          <li>Attack: {attack}</li>
+          <li>Defense: {defense}</li>
+        </ul>
+
+        <div>
+          {types.join(" / ")}
+        </div>
+      </div>
+    );
+
+}
+
+
+export default Pokemon
