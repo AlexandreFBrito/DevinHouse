6826b1e7f67ab328b9c174f757398015d9f556b5
AlexandreFBrito <alexandre_f_brito@estudante.sesisenai.org.br>
2022-01-06 21:46:56 -0300

    Exercicio 10

--- ExerciciosTrello/Semana5/SP05EX10.js
+++ ExerciciosTrello/Semana5/SP05EX10.js
@@ -19,13 +19,20 @@ const produto3 = new Produto("Notebook LG", "notebook.jpg", 10);
 const produtosCadastrados = Produto.cadProdutos([produto1, produto2, produto3]);
 
 function listarProdutos(){
+    
+
     for (let i = 0; i < produtosCadastrados.length; i++) {
-        const nome = produtosCadastrados[i].nome;
-        const img = produtosCadastrados[i].img;
-        const parcelas = produtosCadastrados[i].parcelas;
-        
-        console.log("Nome: "+nome+" imagem: "+img+" parcelas: "+parcelas);
+        let lista = document.getElementById('listaProdutos');
+        let numeroLinhas = lista.rows.length;
+        let linha = lista.insertRow(numeroLinhas);
+        let celula1 = linha.insertCell(0);
+        let celula2 = linha.insertCell(1);
+        let celula3 = linha.insertCell(2);
+
+        celula1.innerHTML = produtosCadastrados[i].nome;
+        celula2.innerHTML = produtosCadastrados[i].img;
+        celula3.innerHTML = `<select><option>${produtosCadastrados[i].parcelas}</option></select>`;
+
     }
 }
 
-listarProdutos();
