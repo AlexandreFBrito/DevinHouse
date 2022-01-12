446c54fc57b4a3bd8d49fc1c514ad6b199904325
AlexandreFBrito <alexandre_f_brito@estudante.sesisenai.org.br>
2022-01-11 22:07:29 -0300

    AUla

---
+++ ReactJS/cardshop/src/pages/Home/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TYPES = [
+  "Aqua",
+  "Beast-Warrior",
+  "Cyberse",
+  "Dinosaur",
+  "Dragon",
+  "Fairy",
+  "Fish",
+  "Insect",
+  "Machine",
+  "Plant",
+  "Rock",
+  "Warrior",
+  "Spellcaster"
+];
+
+function Home() {
+  return (
+    <div className='container-app'>
+      
+        {TYPES.map((type) => 
+        <div className='category-item'>
+        <Link to={`/Cards/${type}`}>{type}</Link>    
+        </div>)}
+      
+    </div>
+
+  );
+}
+
+export default Home;
+
