f124dbb72c10990426fe92111c1c0ab5965b3853
AlexandreFBrito <alexandre_f_brito@estudante.sesisenai.org.br>
2022-02-02 22:12:44 -0300

    Chart

--- ReactJS/menulateral/src/App.js
+++ ReactJS/menulateral/src/App.js
@@ -1,12 +1,13 @@
 import { Dashboard } from '@mui/icons-material';
 import './App.css';
+import Chart from './Components/Chart';
 import Menu from "./Components/Menu";
 
 function App() {
   return (
     <div className="App">
       <Menu></Menu>
-      <Dashboard></Dashboard>
+        <Chart></Chart>
     </div>
   );
 }
